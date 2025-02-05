eea2799aeb0c4535c5d21bb4d49d1d32ef08e825
emerySigma <emeryathallah35@gmail.com>
2025-02-05 01:42:24 +0000

    Submit

---
+++ pytho/velocityCalculator.py
@@ -0,0 +1,10 @@
+#Distance and time value
+distance = float(input('Enter the distance (in meters): '))
+time = float(input('Enter the time (in seconds): '))
+
+#Velocity Formula
+velocity = distance / time
+
+#velocity output
+print(f"The velocity is {velocity:.2f} m/s")
+
